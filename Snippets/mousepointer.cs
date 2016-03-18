60e15c21a36fcabb49690e8eb3a9ba84bec1fb6f
Kokodoko <benni_jolink@hotmail.com>
2016-03-18 15:03:15 +0100

    cursor

--- Snippets/mousepointer.cs
+++ Snippets/mousepointer.cs
@@ -1,4 +1,4 @@
-// we maken de cursor texture static zodat iedereen de texture kan aanpassen
+// cursor texture en positie
 public Texture2D Cursor { get; set; }
 public Vector2 CursorPos;
 
@@ -11,19 +11,10 @@ public void Update(GameTime gameTime)
 {
 	// muispositie uitlezen
 	CursorPos = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
-}
 
-<<<<<<< HEAD
-		// verandering om merge conflict te veroorzaken
-		// dit moet dus weg
-		spriteBatch.Draw(error);
-
-	}
 }
-=======
 public void Draw(SpriteBatch spriteBatch)
 {
 	// teken de cursor op de muispositie
 	spriteBatch.Draw(Cursor, CursorPos, Color.White);
 }
->>>>>>> b785924bc64d7e7651def5b022d89930b232c28d
