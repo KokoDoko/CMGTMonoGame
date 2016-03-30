0aa7c9f2a70c329531db41eabed8d5136b22a49f
bpikaar <bpikaar@users.noreply.github.com>
2016-03-30 20:34:46 +0200

    Update drawbox.cs

--- Snippets/drawbox.cs
+++ Snippets/drawbox.cs
@@ -1,4 +1,5 @@
 Texture2D box = new Texture2D(Game1.instance.GraphicsDevice, 1, 1);
 box.SetData(new[] { Color.Yellow });
 
-Game1.spriteBatch.Draw(_debugbox, new Rectangle(10,10,200,200), Color.White);
+// Voor de rectangle kan de BoundingBox van het object gebruikt worden.
+Game1.spriteBatch.Draw(box, new Rectangle(10,10,200,200), Color.White);
